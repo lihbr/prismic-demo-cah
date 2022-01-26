9e48f986ee19be57ad77f46214d5cac1e680b1c4
lihbr <lihbr@users.noreply.github.com>
2022-01-26 17:08:58 +0100

    chore: save slices to prismic

--- .slicemachine/assets/slices/TextSection/index.stories.js
+++ .slicemachine/assets/slices/TextSection/index.stories.js
@@ -18,7 +18,7 @@ export const _DefaultSlice = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default-slice","name":"Default slice","slice_type":"text_section","items":[],"primary":{"text":[{"type":"paragraph","text":"Quis officia laboris fugiat.","spans":[]},{"type":"paragraph","text":"Id velit ad nisi sint cupidatat mollit in elit ipsum pariatur proident deserunt laborum adipisicing. Deserunt ipsum officia nulla fugiat deserunt dolor enim.","spans":[]}],"background":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.prismic.io/tmp-test-cah/3c2c8174-bc81-46a1-98cf-4c1cbedb6b38_background.png?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}
+      mock: {"variation":"default-slice","name":"Default slice","slice_type":"text_section","items":[],"primary":{"text":[{"type":"paragraph","text":"Sit dolor Lorem amet velit.","spans":[]}],"background":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1504198070170-4ca53bb1c1fa?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
@@ -37,7 +37,7 @@ export const _WithCta = () => ({
   },
   data() {
     return {
-      mock: {"variation":"withCta","name":"WithCTA","slice_type":"text_section","items":[{"link":{"link_type":"Web","url":"http://twitter.com"},"label":"incubate rich action-items"},{"link":{"link_type":"Web","url":"https://prismic.io"},"label":"whiteboard user-centric models"},{"link":{"link_type":"Web","url":"https://prismic.io"},"label":"embrace open-source channels"}],"primary":{"text":[{"type":"heading2","text":"Productize world-class mindshare","spans":[]},{"type":"paragraph","text":"Adipisicing commodo veniam quis tempor do excepteur adipisicing cillum fugiat non. Amet incididunt elit ipsum non nulla excepteur non aute non sint ex. Ea aliquip laboris sit nisi exercitation.","spans":[]}]},"id":"_WithCta"}
+      mock: {"variation":"withCta","name":"WithCTA","slice_type":"text_section","items":[{"link":{"link_type":"Web","url":"http://google.com"},"label":"syndicate value-added initiatives"},{"link":{"link_type":"Web","url":"http://google.com"},"label":"harness innovative supply-chains"},{"link":{"link_type":"Web","url":"https://prismic.io"},"label":"embrace transparent e-commerce"}],"primary":{"text":[{"type":"paragraph","text":"Do incididunt eu mollit est anim aute nostrud sunt voluptate reprehenderit qui.","spans":[]}]},"id":"_WithCta"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
